7a124a2905922427fdc57e2391ba7b140311949f
Mr Sam Law <sam@chongland.com>
2021-03-20 21:08:15 +1100

    Initial HTML & JS files

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Element Selectors
+
+// Variables
+
+// Functions
+function initGame() {
+
+}
+
+function startGame() {
+
+}
+
+function endGame() {
+
+}
+
+// Main game
+function playGame() {
+    initGame();
+    startGame();
+    endGame();
+
+}
+
