4990e84559b723635f55bdaacd46278d60c685dc
Mr Sam Law <sam@chongland.com>
2021-03-21 17:01:47 +1100

    Timer setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 // Element Selectors
-
+var timerElement = document.querySelector(".timer-count");
+var startButton = document.querySelector(".start-button");
 // Variables
+
+var timer;
+var timerCount;
+var qLimit;
+
+const questionsArr = [
+    {
+        question: "", option01: "", option02: "", option03: "", option04: "", correct: ""
+    }, {
+        question: "", option01: "", option02: "", option03: "", option04: "", correct: ""
+    }, {
+        question: "", option01: "", option02: "", option03: "", option04: "", correct: ""
+    }, {
+        question: "", option01: "", option02: "", option03: "", option04: "", correct: ""
+    }, {
+        question: "", option01: "", option02: "", option03: "", option04: "", correct: ""
+    }, {
+        question: "", option01: "", option02: "", option03: "", option04: "", correct: ""
+    }];
 
 // Functions
 function initGame() {
@@ -8,18 +28,44 @@ function initGame() {
 }
 
 function startGame() {
-
+    timerCount = 5;
+    startTimer();
 }
 
 function endGame() {
 
 }
 
+function startTimer() {
+    // Sets Timer
+    timer = setInterval(function () {
+        timerCount--;
+        timerElement.textContent = timerCount;
+        if (timerCount >= 0) {
+            // Tests if win condition is met
+            if (qLimit && timerCount > 0) {
+                // Clears interval and stops timer
+                clearInterval(timer);
+                endGame();
+            }
+        }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+}
+
 // Main game
 function playGame() {
-    initGame();
+
+    // initGame();
     startGame();
-    endGame();
+    // endGame();
 
 }
 
+// Event listener for start button
+startButton.addEventListener("click", playGame);
